aa21e7e332fd1a2bdb402a5785c15aa37f9795be
Ajuogaaz <brianlinus1753@gmail.com>
2020-06-15 08:14:34 -0500

    Adding user and getting gravatar

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const {
     check,
     validationResult
-} = require('express-validator/check');
+} = require('express-validator');
 
-
+//Import user model
+const User = require('../../models/User')
 
 //@route  post api/users
 //@desc   Register Route
@@ -21,13 +24,67 @@ router.post('/', [
             min: 6
         })
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({
                 errors: errors.array()
             })
         }
+
+        const {
+            name,
+            email,
+            password
+        } = req.body;
+
+        try {
+
+            // see if user exist 
+            let user = await User.findOne({
+                email
+            });
+
+            if (user) {
+                return res.status(400).json({
+                    error: [{
+                        msg: 'User already configured'
+                    }]
+                })
+            }
+
+            // Get user gravator
+            const avatar = gravatar.url(email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            })
+
+            user = new User({
+                name,
+                email,
+                avatar,
+                password
+            })
+
+            //Encrypt the password 
+            const salt = await bcrypt.genSalt(10);
+
+            user.password = await bcrypt.hash(password, salt)
+
+            await user.save();
+
+            //Return jsonwebtoken
+            res.send('user registered');
+
+        } catch (err) {
+
+            console.log(err.message);
+            res.status(500).send('Server error')
+
+        }
+
+
         res.send('User route')
     });
 
