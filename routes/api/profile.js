561e74dc48d225995fcb72b0f03e51da2b9749fc
Ajuogaaz <brianlinus1753@gmail.com>
2020-06-16 23:24:44 -0500

    nothing

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -261,9 +261,11 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
         });
 
         //get remove index
-        const removeIndex = profile.experience.filter((item) => item._id.toString() !== req.params.exp_id);
+        const removeIndex = profile.experience.filter(item => item.id).indexOf(req.params.exp_id);
 
         profile.experience.splice(removeIndex, 1);
+
+
 
         await profile.save();
 
