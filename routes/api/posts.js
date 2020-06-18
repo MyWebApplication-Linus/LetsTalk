0ae8713075f0604a4b90903228875576a6be42fe
Ajuogaaz <brianlinus1753@gmail.com>
2020-06-17 19:11:32 -0500

    get all post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -46,5 +46,29 @@ router.post('/', [auth, [
 
 });
 
+//@route  GET api/post
+//@desc   Create new Post
+//@access  private
+
+router.get('/', auth, async (req, res) => {
+
+    try {
+        const posts = await Post.find().sort({
+            date: -1
+        });
+        res.json(posts);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+
+});
+
+//@route  GET api/post
+//@desc   Create new Post
+//@access  private
+
+
 
 module.exports = router;
